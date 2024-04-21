c2406e13cde4b087c6bf72778fac4e125aad98ae
Manish Kumar <91687469+manish8472@users.noreply.github.com>
2024-04-21 18:06:44 +0530

    Update index.ts
    change port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,6 @@ import { connectToDatabase } from './db/connections.js';
 const PORT = process.env.PORT || 5000;
 connectToDatabase() 
 .then(() =>{
-app.listen("https://manish8472.github.io/ai-chatgpt/", () => console.log(""));
+app.listen(PORT, () => console.log(""));
 })
 .catch((err) => console.log(err)) 
